614fc6662e9530d7d4996df8501f8d6ebfc91b99
Eden Chen <chened11@oocl.com>
2024-11-21 14:23:37 +0800

    feat: implement step3, class Kclass

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,7 +1,10 @@
 package oo;
 
 
+import java.util.Objects;
+
 public class Student extends Person {
+    private Klass kclass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
@@ -9,6 +12,14 @@ public class Student extends Person {
 
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %d years old. I am a student.", name, age);
+        return super.introduce() + String.format(" I am a student. I am in class %d.", kclass.getNumber());
+    }
+
+    public void join(Klass targetClass) {
+        kclass = targetClass;
+    }
+
+    public boolean isIn(Klass targetClass) {
+        return Objects.equals(kclass, targetClass);
     }
 }
